fc4ae5dcc68dae3416913106591301d413e8ec33
Btryx <beatryx15@gmail.com>
2025-06-28 21:22:27 +0200

    Started project list component

--- frontend/frontend/src/app/app.routes.ts
+++ frontend/frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
+  { path: 'projects', component: ProjectListComponent},
   { path: '', redirectTo: 'login', pathMatch: 'full' },
  // { path: '**', component: PageNotFoundComponent },
 ];
--- frontend/frontend/src/app/app.ts
+++ frontend/frontend/src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Auth } from './auth.service';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
   protected title = 'Taskii';
+  private authService = inject(Auth);
+  private router = inject(Router);
+
+  isLoggedIn() : boolean {
+      return this.authService.isUserLoggedIn()
+  }
+  logOut() {
+    this.authService.logOut()
+    this.router.navigate(["/login"]);
+  }
 }
